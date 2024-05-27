8b3ce0e023212f95f4cf385313ff9bc5adf8a31b
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2024-05-27 19:50:51 -0400

    Finish with DB

--- demo/src/main/java/com/example/demo/run/RunRepository.java
+++ demo/src/main/java/com/example/demo/run/RunRepository.java
@@ -2,8 +2,13 @@ package com.example.demo.run;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface RunRepository extends ListCrudRepository<Run, Integer> {
-    List<Run> findAllByLocation(String location);
+
+    @Query("SELECT r FROM Run r WHERE r.location = :location")
+    List<Run> findAllByLocation(@Param("location") String location);
+
 }
